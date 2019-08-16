bf8049a78585bc58af92cbc10f467f5bf97734d0
dk <dk@feinian.me>
2019-08-16 15:03:53 +0800

    Add ResponseEnvelopes

---
+++ Niusys.Extensions/ResponseEnvelopes/EnvelopMessage.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+
+namespace Niusys.Extensions.ResponseEnvelopes
+{
+    public class EnvelopMessage
+    {
+        [JsonIgnore]
+        public const string DEFAULT_ERR_MSG = "后端请求处理异常";
+
+        public EnvelopMessage() : this(200, null, null)
+        {
+        }
+
+        public EnvelopMessage(int code, string errorMessage = null, string friendlyMessage = null)
+        {
+            Code = code;
+            //todo: 后续增加code安全胡强制转换
+            ErrorMessage = errorMessage;
+            FriendlyMessage = friendlyMessage;
+        }
+
+        /// <summary>
+        /// 处理状态代码
+        /// </summary>
+        [JsonProperty("code")]
+        public int Code { get; set; }
+
+        /// <summary>
+        /// 处理状态代码对应描述，可展示给最终用户
+        /// </summary>
+        [JsonProperty("msg")]
+        public string FriendlyMessage { get; set; }
+
+        /// <summary>
+        /// 程序详细异常信息，不可展示给最终用户
+        /// </summary>
+        [JsonProperty("err_msg")]
+        public string ErrorMessage { get; set; }
+
+        /// <summary>
+        /// 1请求跟踪代码
+        /// </summary>
+        [JsonProperty("tid")]
+        public string Tid { get; set; }
+
+        /// <summary>
+        /// 固定传string.Empty(根据接口约定)
+        /// </summary>
+        [JsonProperty("data")]
+        public string Data { get; set; } = string.Empty;
+    }
+}
---
+++ Niusys.Extensions/ResponseEnvelopes/EnvelopMessage~T.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+
+namespace Niusys.Extensions.ResponseEnvelopes
+{
+    public class EnvelopMessage<T> : EnvelopMessage
+    {
+        public EnvelopMessage()
+        {
+
+        }
+
+        public EnvelopMessage(int code, string errorMessage, string friendlyMessage, T data)
+            : base(code, errorMessage, friendlyMessage)
+        {
+            Data = data;
+        }
+
+        public EnvelopMessage(T data)
+        {
+            Data = data;
+        }
+
+        [JsonProperty(PropertyName = "data")]
+        public new T Data { get; set; }
+    }
+}

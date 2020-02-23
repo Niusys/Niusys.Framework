3b04d29b1d96f62ea1fb3623be3746c5e784d82a
dk <dk@feinian.me>
2020-02-23 23:38:59 +0800

    Add IsAjaxRequest

--- Niusys.Extensions.AspNetCore/Extensions/HttpContextExtensions.cs
+++ Niusys.Extensions.AspNetCore/Extensions/HttpContextExtensions.cs
@@ -1,11 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text.RegularExpressions;
-using System.Threading.Tasks;
 
 namespace Niusys.Extensions.AspNetCore.Extensions
 {
@@ -27,6 +23,24 @@ namespace Niusys.Extensions.AspNetCore.Extensions
             }
 
             return _apiRequestPattern.IsMatch(httpRequest.Path);
+        }
+
+        /// <summary>
+        /// Determines whether the specified HTTP request is an AJAX request.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// true if the specified HTTP request is an AJAX request; otherwise, false.
+        /// </returns>
+        /// <param name="request">The HTTP request.</param><exception cref="T:System.ArgumentNullException">The <paramref name="request"/> parameter is null (Nothing in Visual Basic).</exception>
+        public static bool IsAjaxRequest(this HttpRequest request)
+        {
+            if (request == null)
+                throw new ArgumentNullException("request");
+
+            if (request.Headers != null)
+                return request.Headers["X-Requested-With"] == "XMLHttpRequest";
+            return false;
         }
 
         public static void SafeAdd<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key, TValue value)

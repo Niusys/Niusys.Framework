a28674a68578464c11b4efc1ca4c079b40ac9282
dk <dk@feinian.me>
2020-02-25 03:15:29 +0800

    Add missing cancellationToken

--- Niusys.Extensions.AspNetCore/Sessions/IUserStore.cs
+++ Niusys.Extensions.AspNetCore/Sessions/IUserStore.cs
@@ -1,12 +1,13 @@
 ﻿using MongoDB.Bson;
 using System;
 using System.Linq.Expressions;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Niusys.Extensions.AspNetCore.Sessions
 {
     public interface IUserStore<TUser>
     {
-        Task<TUser> GetByPropertyAsync<TField>(Expression<Func<TUser, TField>> expression, TField value);
+        Task<TUser> GetByPropertyAsync<TField>(Expression<Func<TUser, TField>> expression, TField value, CancellationToken cancellationToken = default);
     }
 }
